8869fb0ccdcb61138c5a3286d61cee85a35a9084
Artur <abezpalyi@gmail.com>
2021-12-27 18:31:20 +0200

    add response mobile

--- src/js/modules/video-control.js
+++ src/js/modules/video-control.js
@@ -16,7 +16,6 @@ const videoControl = () => {
     if (videoState) {
       video.pause();
       videoState = !videoState;
-
       btn.innerHTML = '<span>Play</span> <svg class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M322.6 520.9V224.2c0-13.5-2.5-28.8 11.8-36.8 15.3-8.6 27.8 1.7 39.6 9.9 139.7 97.5 279.5 195.1 419 292.9 35 24.5 34.8 35.3-0.1 59.7-138.2 96.8-276.5 193.4-414.8 290-5.6 3.9-11.3 7.8-17.4 10.6-22.9 10.4-37 2.4-37.8-22.6-0.9-29.8-0.3-59.7-0.3-89.5V520.9z"  /></svg>';
     } else {
       video.play();
