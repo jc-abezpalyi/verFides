f0fcc423da2510938380dea2374a1ec4ae79a7f8
Artur <abezpalyi@justcoded.com>
2022-01-05 17:39:52 +0200

    fix

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -7,19 +7,14 @@ const burger = () => {
   if (!$btn.length) {
     return;
   }
-
-  const $navList = $('.nav__list');
-
+  
   const OPENED_MENU_CLASSNAME = 'nav-open';
-  const BURGER_OPEN_LIST = 'nav__list-burger';
 
   const burgerHandler = () => {
     $body.toggleClass(OPENED_MENU_CLASSNAME);
-    $navList.toggleClass(BURGER_OPEN_LIST);
-    $btn.toggleClass('opened');
   };
 
-  $btn.click(() => burgerHandler());
+  $btn.click(burgerHandler);
 };
 
 export default burger;
